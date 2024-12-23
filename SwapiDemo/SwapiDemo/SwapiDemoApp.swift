af2fadf61e677f290092caa101a6bb939543f1b7
Fernando Putallaz <ferhedio@gmail.com>
2024-12-23 09:47:16 -0300

    Project created

---
+++ SwapiDemo/SwapiDemo/SwapiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapiDemoApp.swift
+//  SwapiDemo
+//
+//  Created by Fernando Putallaz on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwapiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
