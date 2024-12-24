59996882d62e22659e9b5f749d4d36c72ac43433
Fernando Putallaz <ferhedio@gmail.com>
2024-12-24 15:04:41 -0300

    Add `CharactersView`

---
+++ SwapiDemo/SwapiDemo/Views/CharactersView.swift
@@ -0,0 +1,21 @@
+//
+//  CharactersView.swift
+//  SwapiDemo
+//
+//  Created by Fernando Putallaz on 24/12/2024.
+//
+
+import SwiftUI
+
+struct CharactersView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("This is the CharactersView")
+        }
+    }
+}
+
+#Preview {
+    CharactersView()
+}
